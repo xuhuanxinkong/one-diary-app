e8314ce01ca18ff28d1cac8d2e73feaf392ac385
xuhuanxinkong <83110554@qq.com>
2026-03-31 22:37:10 +0800

    开始

--- app/src/main/java/com/xinkong/diary/ui/screen/chat/GroupSettingScreen.kt
+++ app/src/main/java/com/xinkong/diary/ui/screen/chat/GroupSettingScreen.kt
@@ -34,7 +34,6 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Slider
-import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
--- app/src/main/java/com/xinkong/diary/ui/screen/chat/SettingScreen.kt
+++ app/src/main/java/com/xinkong/diary/ui/screen/chat/SettingScreen.kt
@@ -16,7 +16,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -24,6 +23,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
